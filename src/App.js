78e3912bb7b6fbddbe33f59d9097d4d4184a7056
Juan Patricio Marroquin <review0312oficial@gmail.com>
2022-01-17 22:37:49 -0500

    Creando estructura inicial en App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
+import { useState } from "react";
+import List from "./components/list.component";
+import Alert from './components/alert.component';
 
 function App() {
+  const [name, setName] = useState('');
+  const [list, setList] = useState('');
+  const [isEditing, setIsEditing] = useState(false);
+  const [editId, setEditId] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('hello');
+  }
+
   return (
-    <div className="App">
-      <h2>Hello World</h2>
-    </div>
+    <section className="section-center">
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
+
+        <h3>Grocery Bud</h3>
+
+        <div className="form-control">
+          <input type="text" className="grocery" placeholder="e.g. eggs" value={name} onChange={(e) => setName(e.target.value)} />
+          <button type="submit" className="submit-btn">
+            { isEditing ? 'Edit' : 'Submit' }
+          </button>
+        </div>
+      </form>
+
+      <div className="grocery-container">
+        <List />
+        <button className="clear-btn">Clear Items</button>
+      </div>
+    </section>
   );
 }
 
